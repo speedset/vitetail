1f5e283fb7b468cb616b499a998e3ce2423508d9
speedset <garciadaniloadrian@gmail.com>
2024-02-27 00:25:34 +0100

    Primer Comit

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./index.html","./src/**/*.{html,js,jsx,ts,tsx}"],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
+  
+  
